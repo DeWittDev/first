54159408cec8e9cc730ebe7ca3108ae2893a1c11
DeWittDev <88995967+DeWittDev@users.noreply.github.com>
2021-12-06 15:51:00 -0800

    large react file 1

---
+++ firstjsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Hello Dojo</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>learn ReactJS</li>
+        <li>Earn Black Belt</li>
+        <li>learn to Paint</li>
+      </ul>
+    </>
+  );
+}
+
+
+export default App;
